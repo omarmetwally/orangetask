76ee353b1eb836012a7c07be0abac5f9c349ceab
Omar <omarmetwally2008@hotmail.com>
2023-08-14 18:54:44 +0300

    HomeScreen

---
+++ app/src/main/java/com/omar/orangetask/NewsApplication.kt
@@ -0,0 +1,5 @@
+package com.omar.orangetask
+
+import android.app.Application
+
+class NewsApplication : Application()
---
+++ app/src/main/java/com/omar/orangetask/ui/NewsActivity.kt
@@ -0,0 +1,82 @@
+package com.omar.orangetask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.omar.orangetask.R
+import com.omar.orangetask.db.ArticleDatabase
+import com.omar.orangetask.repository.NewsRepository
+import kotlinx.android.synthetic.main.activity_news.*
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
+
+
+  /*      if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+
+
+        switchtheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                setTheme(R.style.darkTheme)
+
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                setTheme(R.style.AppTheme)
+            }
+
+
+        }*/
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+/*
+        //  force enabling night mode
+       // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+
+        // force disabling night mode
+        // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+
+*/
+
+        // Night mode following system settings
+       delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        print("")
+//        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
+//            setTheme(R.style.darkTheme)
+//        } else {
+//            setTheme(R.style.AppTheme)
+//        }
+
+
+
+
+
+    }
+
+}
