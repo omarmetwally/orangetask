eb97a3e5b25ec227da9adb5c30b7d9d974d88681
Omar <omarmetwally2008@hotmail.com>
2023-08-16 18:26:34 +0300

    Localization

--- app/src/main/java/com/omar/orangetask/NewsApplication.kt
+++ app/src/main/java/com/omar/orangetask/NewsApplication.kt
@@ -1,5 +1,10 @@
 package com.omar.orangetask
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import java.util.Locale
+
 
 class NewsApplication : Application()
--- app/src/main/java/com/omar/orangetask/ui/NewsActivity.kt
+++ app/src/main/java/com/omar/orangetask/ui/NewsActivity.kt
@@ -19,13 +19,20 @@ import java.util.Locale
 class NewsActivity : AppCompatActivity() {
 
     lateinit var viewModel: NewsViewModel
-    var flag=true
+    var flagMode = true
+     var  flagLang="en"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
         super.onCreate(savedInstanceState)
+        flagLang=isLang()!!
+        if(flagLang=="en")
+        {
+            setLocale(this, "en")
+        }
+        else{
+            setLocale(this, "ar")
+        }
+
         setContentView(R.layout.activity_news)
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
@@ -35,59 +42,66 @@ class NewsActivity : AppCompatActivity() {
 
 
 
-       if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            setTheme(R.style.darkTheme)
-        } else {
-            setTheme(R.style.AppTheme)
-        }
 
-        flag = isLightMode()
-        if (flag) {
+
+        flagMode = isLightMode()
+        if (flagMode) {
             floatingthemeswitch.setImageResource(R.drawable.thumbtrue)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            setTheme(R.style.AppTheme)
 
         } else {
             floatingthemeswitch.setImageResource(R.drawable.thumbfalse)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            setTheme(R.style.darkTheme)
         }
 
 
 
-
-        floatingthemeswitch.setOnClickListener {
-            setLocale(this, "ar")
-
-
-
-
-
-
-             if (flag) {
-                 floatingthemeswitch.setImageResource(R.drawable.thumbfalse)
-                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                 setTheme(R.style.darkTheme)
-                 flag = false
-                 saveFlagState(false)
-             } else {
-                 floatingthemeswitch.setImageResource(R.drawable.thumbtrue)
-                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                 setTheme(R.style.AppTheme)
-                 flag = true
-                 saveFlagState(true)
-             }
-
-        }
-
-     /*   switchtheme.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                setTheme(R.style.darkTheme)
-
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                setTheme(R.style.AppTheme)
+        //For language button(Floating)
+        floatinglang.setOnClickListener {
+            if (flagLang=="en"){
+                setLocale(this, "ar")
+                saveFlagLangState("ar")
+            }
+            else{
+                setLocale(this, "en")
+                saveFlagLangState("en")
             }
 
+        }
 
-        }*/
+        //For Dark&Light mode button(Floating)
+        floatingthemeswitch.setOnClickListener {
+
+            if (flagMode) {
+                floatingthemeswitch.setImageResource(R.drawable.thumbfalse)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                setTheme(R.style.darkTheme)
+                flagMode = false
+                saveFlagState(false)
+            } else {
+                floatingthemeswitch.setImageResource(R.drawable.thumbtrue)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                setTheme(R.style.AppTheme)
+                flagMode = true
+                saveFlagState(true)
+            }
+
+        }
+
+        /*   switchtheme.setOnCheckedChangeListener { _, isChecked ->
+               if (isChecked) {
+                   AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                   setTheme(R.style.darkTheme)
+
+               } else {
+                   AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                   setTheme(R.style.AppTheme)
+               }
+
+
+           }*/
 
 
     }
@@ -95,17 +109,17 @@ class NewsActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-/*
-        //  force enabling night mode
-       // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        /*
+                //  force enabling night mode
+               // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
 
-        // force disabling night mode
-        // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+                // force disabling night mode
+                // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
 
-*/
+        */
 
         // Night mode following system settings
-      // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        // delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
 
         print("")
@@ -114,9 +128,6 @@ class NewsActivity : AppCompatActivity() {
 //        } else {
 //            setTheme(R.style.AppTheme)
 //        }
-
-
-
 
 
     }
@@ -131,30 +142,48 @@ class NewsActivity : AppCompatActivity() {
 
     private fun isLightMode(): Boolean {
         val preferences = getSharedPreferences("theme_prefs", MODE_PRIVATE)
-        return preferences.getBoolean("is_light_mode", true) // Assuming default is true (light mode)
+        return preferences.getBoolean(
+            "is_light_mode",
+            true
+        )
     }
 
+
+
+    private fun saveFlagLangState(isLang: String) {
+        val sharedPreferences = getSharedPreferences("app_settings", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("app_language", isLang)
+        editor.apply()
+    }
+
+    private fun isLang(): String? {
+        val sharedPreferences = getSharedPreferences("app_settings", MODE_PRIVATE)
+        var lang= sharedPreferences.getString("app_language", "en")
+        return lang
+    }
+
+
+
+
+
+
+
     fun setLocale(activity: Activity, languageCode: String) {
+
         val locale = Locale(languageCode)
         Locale.setDefault(locale)
-
         val config = Configuration(activity.resources.configuration)
         config.setLocale(locale)
-
         activity.resources.updateConfiguration(
             config, activity.resources.displayMetrics
         )
-
-        // Store the selected language in Shared Preferences or another persistent storage for subsequent launches
-        val sharedPreferences = activity.getSharedPreferences("app_settings", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("app_language", languageCode)
-        editor.apply()
+          activity.recreate() // Recreate the activity to apply changes
+        //restartApp(activity)
 
 
-       // activity.recreate() // Recreate the activity to apply changes
-     // restartApp(activity)
     }
+
     fun restartApp(context: Context) {
         val intent = context.packageManager
             .getLaunchIntentForPackage(context.packageName)
