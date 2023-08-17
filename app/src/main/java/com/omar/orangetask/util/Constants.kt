8a74048df8b6a5a6b819a30ba5bd27f7e22c7030
Omar <omarmetwally2008@hotmail.com>
2023-08-17 10:12:54 +0300

    Handling API_KEY Error

--- app/src/main/java/com/omar/orangetask/util/Constants.kt
+++ app/src/main/java/com/omar/orangetask/util/Constants.kt
@@ -2,9 +2,9 @@ package com.omar.orangetask.util
 
 class Constants {
     companion object {
-        const val API_KEY = "ba31d268e46f4f79b47494e426759d38"
+        const val API_KEY = "95d87d943fc5485ba546a9664eee2a0d"
         const val BASE_URL = "https://newsapi.org"
-        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val SEARCH_NEWS_TIME_DELAY = 50L
         const val QUERY_PAGE_SIZE = 20
     }
 }
