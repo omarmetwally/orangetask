ded31b680d143f5b4061c077b51cb87c46cb1b35
Omar <omarmetwally2008@hotmail.com>
2023-08-17 14:21:24 +0300

    New API_KEY for submission

--- app/src/main/java/com/omar/orangetask/util/Constants.kt
+++ app/src/main/java/com/omar/orangetask/util/Constants.kt
@@ -2,7 +2,7 @@ package com.omar.orangetask.util
 
 class Constants {
     companion object {
-        const val API_KEY = "95d87d943fc5485ba546a9664eee2a0d"
+        const val API_KEY = "796d436e91c34738a3f9deb314907098"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 50L
         const val QUERY_PAGE_SIZE = 20
